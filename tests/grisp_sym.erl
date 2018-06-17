4971b902dabcff7229271d1eb2574780c499137b
aleklisi <sebaczechu123@gmail.com>
2018-06-17 23:43:56 +0200

    adding GRiSP simulating app

---
+++ tests/grisp_sym.erl
@@ -0,0 +1,32 @@
+-module(grisp_sym).
+-author('AleksanderLisiecki').
+
+-export([start/2]).
+
+start(Processes,Reguests) when Processes > 0-> 
+    spawn(fun() -> send_request(Reguests) end),
+    start(Processes-1,Reguests);
+start(_,_) -> ok.
+
+% cd("C:/Users/sebac/Documents/GitHub/2018.05_erlang/tests").
+send_request(N) when N > 0 -> 
+    Temp = rand:uniform(20) + 5,
+    Hum = rand:uniform(100),
+    Request = lists:flatten(
+        io_lib:format(
+            "http://192.168.0.101:8080/erl/web_server:send_measurement?{{temperature,~p},{humidity,~p}}",
+            [Temp,Hum])),
+    %io:fwrite("~p\n",[Request]),
+    %timer:sleep(1001),
+    httpc:request(get, {Request, []}, [], []),
+    send_request(N-1);
+send_request(0) -> 
+    Request = lists:flatten(
+    io_lib:format(
+        "http://192.168.0.101:8080/erl/web_server:send_measurement?{{temperature,~p},{humidity,~p}}",
+        [-1,5])),
+        
+httpc:request(get, {Request, []}, [], []),
+    ok;
+send_request(_) -> ok.
+
